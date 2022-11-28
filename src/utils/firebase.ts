ee27d7bc3d2a7a25d2066fe128bfc45d619dd5c7
khatuantran <khatuantran11@gmail.com>
2022-11-28 23:04:14 +0700

    fix bug firebase

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,20 +1,20 @@
-// Import the functions you need from the SDKs you need
-import "dotenv/config";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import "dotenv/config";
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "advanced-web-apis.firebaseapp.com",
-  projectId: "advanced-web-apis",
-  storageBucket: "advanced-web-apis.appspot.com",
-  messagingSenderId: "94163554845",
-  appId: "1:94163554845:web:0cae8845013c3c0e6d6cf4",
-  measurementId: "G-0VHD8P1N60",
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: "advanced-web-apis.firebaseapp.com",
+//   projectId: "advanced-web-apis",
+//   storageBucket: "advanced-web-apis.appspot.com",
+//   messagingSenderId: "94163554845",
+//   appId: "1:94163554845:web:0cae8845013c3c0e6d6cf4",
+//   measurementId: "G-0VHD8P1N60",
+// };
 
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// export const firebaseApp = initializeApp(firebaseConfig);
